9e86bb090255e56051efd9f1d09c4dff66dcb2a4
Maxim <kuzalkovmaxim@gmail.com>
2023-01-31 16:46:20 +0100

    Dodałem patyczaka który potrafi biegać i skakać.

--- patyczakgra.py
+++ patyczakgra.py
@@ -100,7 +100,7 @@ def kolizja_góra(wsp1, wsp2):
 
 def kolizja_dól(y, wsp1, wsp2):
     if na_osi_x(wsp1, wsp2):
-        oblicz_y = wsp1 + y
+        oblicz_y = wsp1.y2 + y
         if oblicz_y >= wsp2.y1 and oblicz_y <= wsp2.y2:
             return True
     return False
@@ -121,6 +121,125 @@ class DuszekPlatforma(Duszek):
         self.obrazek = obrazek
         self.image = gra.płotno.create_image(x, y, image=self.obrazek, anchor='nw')
         self.współrzędne = Coords(x, y, x + szerokość, y + wysokość)
+
+class DuszekPatyczak(Duszek):
+    def __init__(self, gra):
+        Duszek.__init__(self, gra)
+        self.obrazki_lewa = [
+            PhotoImage(file="patyczak-L1.gif"),
+            PhotoImage(file="patyczak-L2.gif"),
+            PhotoImage(file="patyczak-L3.gif")
+        ]
+        self.obrazki_prawa = [
+            PhotoImage(file="patyczak-P1.gif"),
+            PhotoImage(file="patyczak-P2.gif"),
+            PhotoImage(file="patyczak-P3.gif")
+        ]
+        self.image = gra.płotno.create_image(50, 440, image=self.obrazki_lewa[0], anchor='nw')
+        self.x = -2
+        self.y = 0
+        self.bieżący_obrazek = 0
+        self.bieżący_obrazek_dodaj = 1
+        self.licznik_skoków = 0
+        self.ostatni_czas = time.time()
+        self.współrzędne = Coords()
+        gra.płotno.bind_all('<KeyPress-Left>', self.obrót_w_lewo)
+        gra.płotno.bind_all('<KeyPress-Right>', self.obrót_w_prawo)
+        gra.płotno.bind_all('<space>', self.skok)
+
+    def obrót_w_lewo(self, zdarzenie):
+        if self.y == 0:
+            self.x = -2
+    
+    def obrót_w_prawo(self, zdarzenie):
+        if self.y == 0:
+            self.x = 2
+    
+    def skok(self, zdarzenie):
+        if self.y == 0:
+            self.y = -4
+            self.licznik_skoków = 0
+
+    def animuj(self):
+        if self.x != 0 and self.y == 0:
+            if time.time() - self.ostatni_czas > 0.1:
+                self.ostatni_czas = time.time()
+                self.bieżący_obrazek += self.bieżący_obrazek_dodaj
+                if self.bieżący_obrazek >= 2:
+                    self.bieżący_obrazek_dodaj = -1
+                if self.bieżący_obrazek <= 0:
+                    self.bieżący_obrazek_dodaj = 1
+        if self.x < 0:
+            if self.y != 0:
+                self.gra.płotno.itemconfig(self.image, image=self.obrazki_lewa[2])
+            else:
+                self.gra.płotno.itemconfig(self.image, image=self.obrazki_lewa[self.bieżący_obrazek])
+        if self.x > 0:
+            if self.y != 0:
+                self.gra.płotno.itemconfig(self.image, image=self.obrazki_prawa[2])
+            else:
+                self.gra.płotno.itemconfig(self.image, image=self.obrazki_prawa[self.bieżący_obrazek])    
+
+    def coords(self):
+        xy = self.gra.płotno.coords(self.image)
+        self.współrzędne.x1 = xy[0]
+        self.współrzędne.y1 = xy[1]
+        self.współrzędne.x2 = xy[0] + 27
+        self.współrzędne.y2 = xy[1] + 30
+        return self.współrzędne 
+
+    def move(self):
+        self.animuj() 
+        if self.y < 0:
+            self.licznik_skoków += 1
+            if self.licznik_skoków > 20:
+                self.y = 4
+        if self.y > 0:
+            self.licznik_skoków -= 1
+        wsp = self.coords()
+        lewa = True
+        prawa = True
+        góra = True
+        dół = True
+        spadanie = True
+        if self.y > 0 and wsp.y2 >= self.gra.wysokość_płotna:
+            self.y = 0
+            dół = False
+        elif self.y < 0 and wsp.y1 <= 0:
+            self.y = 0
+            góra = False
+        if self.x > 0 and wsp.x2 >= self.gra.szerokość_płotna:
+            self.x = 0
+            prawa = False
+        elif self.x < 0 and wsp.x1 <= 0:
+            self.x = 0 
+            lewa = False
+        for duszek in self.gra.duszki:
+            if duszek == self:
+                continue
+            duszek_wsp = duszek.coords()
+            if góra and self.y < 0 and kolizja_góra(wsp, duszek_wsp):
+                self.y = -self.y
+                góra = False
+            if dół and self.y > 0 and kolizja_dól(self.y, wsp, duszek_wsp):
+                self.y = duszek_wsp.y1 - wsp.y2
+                if self.y < 0:
+                    self.y = 0
+                dół = False
+                góra = False
+            if (dół) and (spadanie) and (self.y == 0) \
+                    and (wsp.y2 < self.gra.wysokość_płotna) \
+                    and (kolizja_dól(1, wsp, duszek_wsp)):
+                spadanie = False
+            if lewa and self.x < 0 and kolizja_lewa(wsp, duszek_wsp):
+                self.x = 0
+                lewa = False
+            if prawa and self.x > 0 and kolizja_prawa(wsp, duszek_wsp):
+                self.x = 0
+                prawa = False
+        if spadanie and dół and self.y == 0 and wsp.y2 < self.gra.wysokość_płotna:
+            self.y = 4
+        self.gra.płotno.move(self.image, self.x, self.y)
 
 g = gra()
 platforma1 = DuszekPlatforma(g, PhotoImage(file="platforma1.gif"), 0, 480, 100, 10)
@@ -143,4 +262,6 @@ g.duszki.append(platforma7)
 g.duszki.append(platforma8)
 g.duszki.append(platforma9)
 g.duszki.append(platforma10)
+sf = DuszekPatyczak(g)
+g.duszki.append(sf)
 g.pętla_główna()
